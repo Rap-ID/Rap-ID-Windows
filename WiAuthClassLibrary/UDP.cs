18476f35a2a9bd3d83de0dc1dc3433cf7e8cbd3c
小傅Fox <xfox@cotr.me>
2015-02-03 17:53:12 +0800

    给UDP提供文档注释

--- WiAuthClassLibrary/UDP.cs
+++ WiAuthClassLibrary/UDP.cs
@@ -9,19 +9,38 @@ namespace WiAuth.ClassLibrary
 {
     public class UDP : INetworkListener
     {
+        /// <summary>
+        /// 底层UDP
+        /// </summary>
         private UdpClient udpClient { get; set; }
+        /// <summary>
+        /// 监听的范围
+        /// </summary>
         private IPEndPoint objIEP;
-        private bool disposed { get; set; }
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="port">端口</param>
         public UDP(int port)
         {
             this.objIEP = new IPEndPoint(IPAddress.Any, port);
             this.udpClient = new UdpClient(this.objIEP);
         }
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="port">端口</param>
         public UDP(Network.Ports port)
             : this((int)port)
         {
         }
+        /// <summary>
+        /// 表示监听状态
+        /// </summary>
         public bool listening { get; set; }
+        /// <summary>
+        /// 接收消息
+        /// </summary>
         private async void Receive()
         {
             var result = await this.udpClient.ReceiveAsync();
@@ -31,17 +50,35 @@ namespace WiAuth.ClassLibrary
             if (listening)
                 Receive();
         }
+        /// <summary>
+        /// 开始监听
+        /// </summary>
         public void StartListen()
         {
             this.listening = true;
             this.Receive();
         }
+        /// <summary>
+        /// 停止监听
+        /// </summary>
         public void StopListen()
         {
             this.listening = false;
         }
+        /// <summary>
+        /// 消息事件处理程序委托
+        /// </summary>
+        /// <param name="sender">事件触发者</param>
+        /// <param name="message">消息</param>
+        /// <param name="remote">远程IP和端口</param>
         public delegate void OnMessageEventHandler(object sender, string message, IPEndPoint remote);
+        /// <summary>
+        /// 消息事件
+        /// </summary>
         public event OnMessageEventHandler OnMessage;
+        /// <summary>
+        /// 关闭服务端
+        /// </summary>
         public void Close()
         {
             this.udpClient.Close();
