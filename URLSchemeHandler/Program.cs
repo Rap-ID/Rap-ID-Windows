d8e8a8498f543cb5cff20dc06db43bf545abe4ac
小傅Fox <xfox@cotr.me>
2015-02-05 21:59:48 +0800

    URLHandler完成
    代码未经测试

---
+++ URLSchemeHandler/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URLSchemeHandler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args[1].StartsWith("authorize&"))
+            {
+                Console.WriteLine("Authorize");
+                var cb = args[1].Replace("authorize&", "");
+                Console.WriteLine("Callback: {0}", cb);
+                System.Diagnostics.Process.Start(AppDomain.CurrentDomain.BaseDirectory + "/auth.exe \"{0}\"", cb);
+            }
+            else
+            {
+                Console.WriteLine("Invaid Call");
+            }
+        }
+    }
+}
