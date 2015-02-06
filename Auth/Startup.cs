5fd502eacb87653a515e44d8822f101b631ae4e3
小傅Fox <xfox@cotr.me>
2015-02-06 23:10:18 +0800

    Auth基本代码

---
+++ Auth/Startup.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WiAuth.ClassLibrary;
+
+namespace WiAuth.AuthUI
+{
+    public partial class Startup : Form
+    {
+        private string callback = String.Empty;
+        Device device;
+        string key;
+        Waiting waitFrm = new Waiting();
+
+        public Startup()
+        {
+            InitializeComponent();
+            this.Hide();
+        }
+
+        public Startup(string callback)
+            : this()
+        {
+            this.callback = callback;
+        }
+
+        private void Startup_Load(object sender, EventArgs e)
+        {
+            new Task(() =>
+            {
+                const string auth_prefix = "AUTH";
+
+                waitFrm.Show();
+                waitFrm.SetInfoText("正在读取配置文件...");
+
+                using (var sr = new System.IO.StreamReader(AppDomain.CurrentDomain.BaseDirectory + "pair"))
+                {
+                    var ss = sr.ReadLine();
+                    device = new Device(ss);
+                    key = sr.ReadLine();
+                }
+                waitFrm.SetInfoText("配置文件成功读取，正在建立连接...");
+
+                var tcpClient = new TCPClient(device.IP, NetworkPorts.Auth);
+                tcpClient.OnMessage += tcpClient_OnMessage;
+                tcpClient.Connect();
+                waitFrm.SetInfoText("连接已建立，正在发送消息...");
+
+                tcpClient.Send(auth_prefix + key);
+                waitFrm.SetInfoText("消息发送成功，正在等待手机端回应...");
+            }).Start();
+        }
+
+        void tcpClient_OnMessage(object sender, string message)
+        {
+            const string authok_prefix = "AUTHOK";
+            const string authfail_prefix = "AUTHFAIL";
+
+#if DEBUG
+            MessageBox.Show(message);
+#endif
+
+            if (message.StartsWith(authok_prefix))
+            {
+                waitFrm.SetInfoText("授权成功！");
+                System.Threading.Thread.Sleep(1000);
+                if (callback != String.Empty)
+                {
+                    System.Diagnostics.Process.Start(callback + message.Replace(authok_prefix, String.Empty));
+                    Application.ExitThread();
+                }
+            }
+            else if (message.StartsWith(authfail_prefix))
+            {
+                waitFrm.SetInfoText("授权失败.");
+                System.Threading.Thread.Sleep(1000);
+                Application.ExitThread();
+            }
+        }
+    }
+}
