5c9e2b6fac69e950d52c02aca6292ff29c912ad9
小傅Fox <xfox@cotr.me>
2015-02-11 21:29:47 +0800

    增加了安装器
    增加了安装器
    取消了URLSchemeHandler的注册Protocol功能

---
+++ Installer/Install.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace WiAuth.Installer
+{
+    partial class Install
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.insButton = new System.Windows.Forms.Button();
+            this.uninsButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dirBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // insButton
+            // 
+            this.insButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.insButton.Location = new System.Drawing.Point(12, 47);
+            this.insButton.Name = "insButton";
+            this.insButton.Size = new System.Drawing.Size(107, 40);
+            this.insButton.TabIndex = 0;
+            this.insButton.Text = "安装";
+            this.insButton.UseVisualStyleBackColor = true;
+            this.insButton.Click += new System.EventHandler(this.insButton_Click);
+            // 
+            // uninsButton
+            // 
+            this.uninsButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.uninsButton.Location = new System.Drawing.Point(125, 47);
+            this.uninsButton.Name = "uninsButton";
+            this.uninsButton.Size = new System.Drawing.Size(111, 40);
+            this.uninsButton.TabIndex = 1;
+            this.uninsButton.Text = "卸载";
+            this.uninsButton.UseVisualStyleBackColor = true;
+            this.uninsButton.Click += new System.EventHandler(this.uninsButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 21);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "目录";
+            // 
+            // dirBox
+            // 
+            this.dirBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dirBox.Location = new System.Drawing.Point(73, 12);
+            this.dirBox.Name = "dirBox";
+            this.dirBox.Size = new System.Drawing.Size(321, 29);
+            this.dirBox.TabIndex = 3;
+            // 
+            // Install
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 99);
+            this.Controls.Add(this.dirBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.uninsButton);
+            this.Controls.Add(this.insButton);
+            this.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Install";
+            this.Text = "WiAuth安装程序";
+            this.Load += new System.EventHandler(this.Install_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button insButton;
+        private System.Windows.Forms.Button uninsButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox dirBox;
+    }
+}
---
+++ Installer/Install.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WiAuth.Installer
+{
+    public partial class Install : Form
+    {
+        public Install()
+        {
+            InitializeComponent();
+        }
+
+        private void Install_Load(object sender, EventArgs e)
+        {
+            this.dirBox.Text = AppDomain.CurrentDomain.BaseDirectory;
+        }
+
+        /// <summary>
+        /// 注册启动项到注册表
+        /// </summary>
+        public void RegURL(string name)
+        {
+            //注册的协议头，即在地址栏中的路径 如QQ的：tencent://xxxxx/xxx 我注册的是jun 在地址栏中输入：jun:// 就能打开本程序
+            var surekamKey = Microsoft.Win32.Registry.ClassesRoot.CreateSubKey(name);
+            //以下这些参数都是固定的，不需要更改，直接复制过去 
+            var shellKey = surekamKey.CreateSubKey("shell");
+            var openKey = shellKey.CreateSubKey("open");
+            var commandKey = openKey.CreateSubKey("command");
+            surekamKey.SetValue("URL Protocol", "");
+            //这里可执行文件取当前程序全路径，可根据需要修改
+            var exePath = this.dirBox.Text + "URLSchemeHandler.exe";
+            commandKey.SetValue("", "\"" + exePath + "\"" + " \"%1\"");
+        }
+
+        /// <summary>
+        /// 取消注册
+        /// </summary>
+        public void UnRegURL(string name)
+        {
+            //直接删除节点
+            Microsoft.Win32.Registry.ClassesRoot.DeleteSubKeyTree(name);
+        }
+
+        private void insButton_Click(object sender, EventArgs e)
+        {
+            RegURL("wiauth");
+        }
+
+        private void uninsButton_Click(object sender, EventArgs e)
+        {
+            UnRegURL("wiauth");
+        }
+    }
+}
