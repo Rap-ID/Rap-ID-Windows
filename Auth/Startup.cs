2790265ed44cdecfc01404347f7094cfbcfcd1c3
小傅Fox <xfox@cotr.me>
2015-02-11 20:23:02 +0800

    使用广播包来侦测手机ip

--- Auth/Startup.cs
+++ Auth/Startup.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using WiAuth.ClassLibrary;
+using System.Net;
+using System.Net.Sockets;
 
 namespace WiAuth.AuthUI
 {
@@ -34,43 +36,21 @@ namespace WiAuth.AuthUI
             waitFrm.Show();
             new Task(() =>
             {
-                const string auth_prefix = "AUTH";
-
-                waitFrm.SetInfoText("正在读取配置文件...");
-
-                using (var sr = new System.IO.StreamReader(AppDomain.CurrentDomain.BaseDirectory + "pair"))
-                {
-                    var ss = sr.ReadLine();
-#if DEBUG
-                    device = new Device("name", System.Net.IPAddress.Parse("127.0.0.1"));
-#else
-                    device = new Device(ss);
-#endif
-                    key = sr.ReadLine();
-                }
-                waitFrm.SetInfoText("配置文件成功读取，正在建立连接...");
-
-                var tcpClient = new TCPClient(device.IP, NetworkPorts.Auth);
-                tcpClient.OnMessage += tcpClient_OnMessage;
-                tcpClient.Connect();
-                waitFrm.SetInfoText("连接已建立，正在发送消息...");
-
-                tcpClient.Send(auth_prefix + key);
-                waitFrm.SetInfoText("消息发送成功，正在等待手机端回应...");
-            }).Start();
-            new Task(() =>
-            {
                 this.OverThread = () =>
                 {
                     this.Hide();
                 };
                 this.Invoke(OverThread);
             }).Start();
+            new Task(async () =>
+            {
+                await AuthProccess();
+            }).Start();
         }
         public delegate void DOverThread();
         public DOverThread OverThread;
 
-        void tcpClient_OnMessage(object sender, string message)
+        void tcpClient_OnMessage_WaitForAuth(object sender, string message)
         {
             const string authok_prefix = "AUTHOK";
             const string authfail_prefix = "AUTHFAIL";
@@ -116,6 +96,46 @@ namespace WiAuth.AuthUI
             while ((newUrl = Uri.UnescapeDataString(url)) != url)
                 url = newUrl;
             return newUrl;
+        }
+
+        private async Task<Device> WaitForIP(string name)
+        {
+            IPAddress ip = IPAddress.Any;
+            using (var udp = new UdpClient(new IPEndPoint(IPAddress.Any, NetworkPorts.Boradcast)))
+            {
+                string message = String.Empty;
+                while (message != name)
+                {
+                    var result = await udp.ReceiveAsync();
+                    message = Encodes.UTF8NoBOM.GetString(result.Buffer);
+                    ip = result.RemoteEndPoint.Address;
+                }
+            }
+            return new Device(name, ip);
+        }
+
+        private async Task AuthProccess()
+        {
+            const string auth_prefix = "AUTH";
+
+            waitFrm.SetInfoText("正在读取配置文件...");
+
+            using (var sr = new System.IO.StreamReader(AppDomain.CurrentDomain.BaseDirectory + "pair"))
+            {
+                var name = sr.ReadLine();
+                device = await WaitForIP(name);
+                //device = new Device("name", "127.0.0.1");
+                key = sr.ReadLine();
+            }
+            waitFrm.SetInfoText("配置文件成功读取，正在建立连接...");
+
+            var tcpClient = new TCPClient(device.IP, NetworkPorts.Auth);
+            tcpClient.OnMessage += tcpClient_OnMessage_WaitForAuth;
+            tcpClient.Connect();
+            waitFrm.SetInfoText("连接已建立，正在发送消息...");
+
+            tcpClient.Send(auth_prefix + key);
+            waitFrm.SetInfoText("消息发送成功，正在等待手机端回应...");
         }
     }
 }
