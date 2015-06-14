1f75d7de6dcbcf2d67929176debbf2780d74f8fa
小傅Fox <coderfox.fu@gmail.com>
2015-06-14 13:36:18 +0800

    Rolling key for authentication

--- Auth/Properties/Settings.Designer.cs
+++ Auth/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace RapID.AuthUI.Properties {
+namespace RapID.Auth.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
